d347f2e50c0f0de0698a3523fc3ab5bec77e825b
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-01-11 23:46:57 +0100

    Revert "Revive ConditionalRecipe and ConditionalAdvancement. Also directly add conditional support to all vanilla recipe data generators."
    This reverts commit 587b6840

--- src/main/java/net/minecraftforge/common/extensions/IForgeGameTestHelper.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeGameTestHelper.java
@@ -6,12 +6,10 @@
 package net.minecraftforge.common.extensions;
 
 import java.util.UUID;
-import java.util.function.Supplier;
 
 import com.mojang.authlib.GameProfile;
 
 import io.netty.channel.embedded.EmbeddedChannel;
-import net.minecraft.gametest.framework.GameTestAssertException;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.network.Connection;
 import net.minecraft.network.ConnectionProtocol;
@@ -23,16 +21,6 @@ import net.minecraft.server.network.ServerGamePacketListenerImpl;
 public interface IForgeGameTestHelper {
     private GameTestHelper self() {
         return (GameTestHelper)this;
-    }
-
-    default void assertTrue(boolean value, Supplier<String> message) {
-       if (!value)
-          throw new GameTestAssertException(message.get());
-    }
-
-    default void assertFalse(boolean value, Supplier<String> message) {
-       if (value)
-          throw new GameTestAssertException(message.get());
     }
 
     default ServerPlayer makeMockServerPlayer() {
