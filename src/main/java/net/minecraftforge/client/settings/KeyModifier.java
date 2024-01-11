55ab664fec444475c0df3f3a35afa943c3a9d2dc
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-01-11 16:33:21 +0100

    Revert "Rework KeyModifiers system to properly allow keybinds to be triggered when multiple modifiers are pressed. Fix setting keybinds whel using keyboard inputs to select the menu. Closes #9793"
    This reverts commit c1557ec3

--- src/main/java/net/minecraftforge/client/settings/KeyModifier.java
+++ src/main/java/net/minecraftforge/client/settings/KeyModifier.java
@@ -5,7 +5,6 @@
 
 package net.minecraftforge.client.settings;
 
-import java.util.List;
 import java.util.function.Supplier;
 
 import net.minecraft.client.Minecraft;
@@ -19,150 +18,102 @@ import org.lwjgl.glfw.GLFW;
 public enum KeyModifier {
     CONTROL {
         @Override
-        public boolean matches(InputConstants.Key key)
-        {
+        public boolean matches(InputConstants.Key key) {
             int keyCode = key.getValue();
             if (Minecraft.ON_OSX)
-            {
                 return keyCode == GLFW.GLFW_KEY_LEFT_SUPER || keyCode == GLFW.GLFW_KEY_RIGHT_SUPER;
-            }
             else
-            {
                 return keyCode == GLFW.GLFW_KEY_LEFT_CONTROL || keyCode == GLFW.GLFW_KEY_RIGHT_CONTROL;
-            }
         }
 
         @Override
-        public boolean isActive(@Nullable IKeyConflictContext conflictContext)
-        {
+        public boolean isActive(@Nullable IKeyConflictContext conflictContext) {
             return Screen.hasControlDown();
         }
 
         @Override
-        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic)
-        {
+        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic) {
             String localizationFormatKey = Minecraft.ON_OSX ? "forge.controlsgui.control.mac" : "forge.controlsgui.control";
             return Component.translatable(localizationFormatKey, defaultLogic.get());
         }
     },
     SHIFT {
         @Override
-        public boolean matches(InputConstants.Key key)
-        {
+        public boolean matches(InputConstants.Key key) {
             return key.getValue() == GLFW.GLFW_KEY_LEFT_SHIFT || key.getValue() == GLFW.GLFW_KEY_RIGHT_SHIFT;
         }
 
         @Override
-        public boolean isActive(@Nullable IKeyConflictContext conflictContext)
-        {
+        public boolean isActive(@Nullable IKeyConflictContext conflictContext) {
             return Screen.hasShiftDown();
         }
 
         @Override
-        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic)
-        {
+        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic) {
             return Component.translatable("forge.controlsgui.shift", defaultLogic.get());
         }
     },
     ALT {
         @Override
-        public boolean matches(InputConstants.Key key)
-        {
+        public boolean matches(InputConstants.Key key) {
             return key.getValue() == GLFW.GLFW_KEY_LEFT_ALT || key.getValue() == GLFW.GLFW_KEY_RIGHT_ALT;
         }
 
         @Override
-        public boolean isActive(@Nullable IKeyConflictContext conflictContext)
-        {
+        public boolean isActive(@Nullable IKeyConflictContext conflictContext) {
             return Screen.hasAltDown();
         }
 
         @Override
-        public Component getCombinedName(InputConstants.Key keyCode, Supplier<Component> defaultLogic)
-        {
+        public Component getCombinedName(InputConstants.Key keyCode, Supplier<Component> defaultLogic) {
             return Component.translatable("forge.controlsgui.alt", defaultLogic.get());
         }
     },
     NONE {
         @Override
-        public boolean matches(InputConstants.Key key)
-        {
+        public boolean matches(InputConstants.Key key) {
             return false;
         }
 
         @Override
-        public boolean isActive(@Nullable IKeyConflictContext conflictContext)
-        {
-            if (conflictContext != null && !conflictContext.conflicts(KeyConflictContext.IN_GAME))
-            {
-                for (KeyModifier keyModifier : MODIFIER_VALUES)
-                {
+        public boolean isActive(@Nullable IKeyConflictContext conflictContext) {
+            if (conflictContext != null && !conflictContext.conflicts(KeyConflictContext.IN_GAME)) {
+                for (KeyModifier keyModifier : MODIFIER_VALUES) {
                     if (keyModifier.isActive(conflictContext))
-                    {
                         return false;
-                    }
                 }
             }
             return true;
         }
 
         @Override
-        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic)
-        {
+        public Component getCombinedName(InputConstants.Key key, Supplier<Component> defaultLogic) {
             return defaultLogic.get();
         }
     };
 
     public static final KeyModifier[] MODIFIER_VALUES = {SHIFT, CONTROL, ALT};
 
-    public static KeyModifier getActiveModifier()
-    {
-        for (KeyModifier keyModifier : MODIFIER_VALUES)
-        {
+    public static KeyModifier getActiveModifier() {
+        for (KeyModifier keyModifier : MODIFIER_VALUES) {
             if (keyModifier.isActive(null))
-            {
                 return keyModifier;
-            }
         }
         return NONE;
     }
 
-    private static final KeyModifier[] VALUES = {SHIFT, CONTROL, ALT};
-    private static final List<KeyModifier> VALUES_LIST = List.of(SHIFT, CONTROL, ALT);
-    private static final List<KeyModifier> ALL = List.of(SHIFT, CONTROL, ALT, NONE);
-    public static final List<KeyModifier> getValues(boolean includeNone) {
-        return includeNone ? ALL : VALUES_LIST;
-    }
-
-    @Nullable
-    public static KeyModifier getModifier(InputConstants.Key key) {
-        for (var modifier : VALUES) {
-            if (modifier.matches(key))
-                return modifier;
-        }
-        return null;
-    }
-
-    public static boolean isKeyCodeModifier(InputConstants.Key key)
-    {
-        for (KeyModifier keyModifier : MODIFIER_VALUES)
-        {
+    public static boolean isKeyCodeModifier(InputConstants.Key key) {
+        for (KeyModifier keyModifier : MODIFIER_VALUES) {
             if (keyModifier.matches(key))
-            {
                 return true;
-            }
         }
         return false;
     }
 
-    public static KeyModifier valueFromString(String stringValue)
-    {
-        try
-        {
+    public static KeyModifier valueFromString(String stringValue) {
+        try {
             return valueOf(stringValue);
-        }
-        catch (NullPointerException | IllegalArgumentException ignored)
-        {
+        } catch (NullPointerException | IllegalArgumentException ignored) {
             return NONE;
         }
     }
