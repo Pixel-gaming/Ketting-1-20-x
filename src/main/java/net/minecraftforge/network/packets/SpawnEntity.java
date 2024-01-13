e29e61c7b6e22857c65fe65aa9034de0f86507aa
C0D3 M4513R <28912031+C0D3-M4513R@users.noreply.github.com>
2024-01-13 19:52:10 +0100

    Revert "1.20.2-pre2"
    This reverts commit 4786fa6f

--- src/main/java/net/minecraftforge/network/packets/SpawnEntity.java
+++ src/main/java/net/minecraftforge/network/packets/SpawnEntity.java
@@ -21,6 +21,7 @@ import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.util.LogicalSidedProvider;
 import net.minecraftforge.entity.IEntityAdditionalSpawnData;
 import net.minecraftforge.event.network.CustomPayloadEvent;
+import net.minecraftforge.event.network.CustomPayloadEvent.Context;
 
 /**
  * Used to spawn a custom entity without the same restrictions as
@@ -40,7 +41,6 @@ public class SpawnEntity {
     private final int velX, velY, velZ;
     private final FriendlyByteBuf buf;
 
-    @SuppressWarnings("deprecation")
     SpawnEntity(Entity e) {
         this.entity = e;
         this.typeId = BuiltInRegistries.ENTITY_TYPE.getId(e.getType()); //TODO: Codecs
@@ -125,7 +125,6 @@ public class SpawnEntity {
     public static void handle(SpawnEntity msg, CustomPayloadEvent.Context ctx) {
         ctx.enqueueWork(() -> {
             try {
-                @SuppressWarnings("deprecation")
                 EntityType<?> type = BuiltInRegistries.ENTITY_TYPE.byId(msg.typeId);
                 Optional<Level> world = LogicalSidedProvider.CLIENTWORLD.get(ctx.getDirection().getReceptionSide());
                 Entity e = world.map(w -> type.customClientSpawn(msg, w)).orElse(null);
@@ -143,8 +142,7 @@ public class SpawnEntity {
 
                 e.setId(msg.entityId);
                 e.setUUID(msg.uuid);
-                if (world.orElse(null) instanceof ClientLevel cworld)
-                    cworld.addEntity(e);
+                world.filter(ClientLevel.class::isInstance).ifPresent(w -> ((ClientLevel) w).m_104627_(msg.entityId, e));
                 e.lerpMotion(msg.velX / 8000.0, msg.velY / 8000.0, msg.velZ / 8000.0);
                 if (e instanceof IEntityAdditionalSpawnData entityAdditionalSpawnData)
                     entityAdditionalSpawnData.readSpawnData(msg.buf);
